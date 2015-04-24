a375049fd997e67d3c897cfcd50456b518952c2a
Pedro Bacchini <engenharia_bacchini@hotmail.com>
2015-04-24 18:47:51 -0300

    Splash Screen

---
+++ Assets/01 Splash Screen/Scripts/SplashScreenController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenController : MonoBehaviour 
+{
+	public float delayMinNextScene = 3.0f;
+	public string nextSceneName = "Main Menu";
+
+	IEnumerator Start () 
+	{
+		//Aproveitar esse tempo para fazer o loading da proxima Cena
+		yield return new WaitForSeconds (delayMinNextScene);
+		GetComponent<NextScene> ().LoadLevel (nextSceneName);
+	}
+}
---
+++ Assets/Outros/Scripts/NextScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextScene : MonoBehaviour 
+{
+	public void LoadLevel(string sceneName)
+	{
+		Application.LoadLevel (sceneName);
+	}
+}
