a70ac42582b34bea9b5c4d8679dc6b3cdeaf4a4b
Pedro Bacchini <engenharia_bacchini@hotmail.com>
2015-04-24 20:52:13 -0300

    Loading Sincrono.

--- Assets/01 Splash Screen/Scripts/SplashScreenController.cs
+++ Assets/01 Splash Screen/Scripts/SplashScreenController.cs
@@ -6,10 +6,33 @@ public class SplashScreenController : MonoBehaviour
 	public float delayMinNextScene = 3.0f;
 	public string nextSceneName = "Main Menu";
 
-	IEnumerator Start () 
+	void Start () 
 	{
-		//Aproveitar esse tempo para fazer o loading da proxima Cena
-		yield return new WaitForSeconds (delayMinNextScene);
-		GetComponent<NextScene> ().LoadLevel (nextSceneName);
+		AsyncOperation status = Application.LoadLevelAsync (nextSceneName);
+
+		status.allowSceneActivation = false;
+		
+		StartCoroutine (LoadLevelProgress (status));
+	}
+	
+	
+	IEnumerator LoadLevelProgress (AsyncOperation status) 
+	{
+		while (!status.isDone && status.progress < 0.9f)
+		{
+			//Debug.Log ("Loading " + status.progress);
+
+			yield return null;
+		}
+
+		float whaitTime = delayMinNextScene - Time.time;
+		
+		//Debug.Log ("Whait Time: "+whaitTime);
+		
+		yield return new WaitForSeconds (whaitTime);
+
+		//Debug.Log ("Loading complete");
+
+		status.allowSceneActivation = true;
 	}
 }
