590fa610f323529f87e3ac2d8a1ebd6d6b49a3ef
Pedro Bacchini <engenharia_bacchini@hotmail.com>
2015-04-25 03:05:03 -0300

    Distancia Percorrida!

---
+++ Assets/03 Gameplay/Scripts/HUDDistance.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HUDDistance : MonoBehaviour 
+{	
+	// Update is called once per frame
+	void Update () 
+	{
+		GetComponent<Text> ().text = Runner.distanceTraveled.ToString();
+	}
+}
