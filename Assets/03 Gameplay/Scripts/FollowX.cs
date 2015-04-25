5d5774a93e75a8692dcc7b2c35c481eb3bd97c25
Pedro Bacchini <engenharia_bacchini@hotmail.com>
2015-04-25 02:25:45 -0300

    First GamePlay!

---
+++ Assets/03 Gameplay/Scripts/FollowX.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowX : MonoBehaviour 
+{
+	public Transform target;
+
+	public float distance = 6;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 newPosition = new Vector3(target.position.x + distance,0,-10);
+		transform.position = newPosition;
+	}
+}
---
+++ Assets/03 Gameplay/Scripts/Runner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Runner : MonoBehaviour 
+{
+	public Scrollbar scrollBar;
+	public float limit = 4.6f;
+
+	void Update () 
+	{
+		Debug.Log (scrollBar.value);
+	
+		transform.Translate(5f * Time.deltaTime, 0f, 0f);
+
+		transform.position = new Vector3 (transform.position.x, -1 * limit + (scrollBar.value * 2 * limit) , transform.position.z);
+	}
+}
