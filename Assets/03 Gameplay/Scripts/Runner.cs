b6ff9899ef46bb66e4a6926a4b1ad09bceb9fbf8
Pedro Bacchini <engenharia_bacchini@hotmail.com>
2015-04-25 05:12:55 -0300

    Velocidade aumentando.

--- Assets/03 Gameplay/Scripts/Runner.cs
+++ Assets/03 Gameplay/Scripts/Runner.cs
@@ -5,13 +5,26 @@ using UnityEngine.UI;
 public class Runner : MonoBehaviour 
 {
 	public Scrollbar scrollBar;
-	public float limit = 4.6f;
-	public float velocity = 5f;
-
 	public static float distanceTraveled;
+	public float limit = 4.6f;
+
+	public float startVelocity = 5f;
+	public float scaleVelocity = 1.001f;
+	public float velocity;
+
+
+
+
+
+	void Start()
+	{
+		velocity = startVelocity;
+	}
 
 	void Update () 
 	{	
+		velocity *= scaleVelocity;
+
 		transform.Translate(velocity * Time.deltaTime, 0f, 0f);
 
 		transform.position = new Vector3 (transform.position.x, -1 * limit + (scrollBar.value * 2 * limit) , transform.position.z);
