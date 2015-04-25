eda9880527d69d7273a3b710b8b4f9e82f167727
Pedro Bacchini <engenharia_bacchini@hotmail.com>
2015-04-25 03:48:01 -0300

    Correcao de bug dar respaw no meio do lv.

--- Assets/03 Gameplay/Scripts/BackgroundController.cs
+++ Assets/03 Gameplay/Scripts/BackgroundController.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class BackgroundController : MonoBehaviour 
 {
 	public GameController game;
+	public float maxDistance = 30.0f;
 
 	// Update is called once per frame
 	void Update () 
 	{
 //		Debug.Log (Runner.distanceTraveled - transform.position.x);
-		if(Runner.distanceTraveled - transform.position.x > 30.0f)
+		if(Runner.distanceTraveled - transform.position.x > maxDistance)
 		{
 			Vector3 newPosition = new Vector3(game.getNextBackgroundPosition(),transform.position.y,transform.position.z);
 			transform.position = newPosition;
