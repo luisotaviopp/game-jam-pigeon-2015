7cfa3d441ba7d73678d998bcb39b61bf33931171
Pedro Bacchini <engenharia_bacchini@hotmail.com>
2015-04-25 06:09:49 -0300

    Canos OnDemand!

--- Assets/03 Gameplay/Scripts/GameController.cs
+++ Assets/03 Gameplay/Scripts/GameController.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class GameController : MonoBehaviour 
 {
-	public float lenghtBackground = 40.0f;
-	float nextBackgroundPosition = 45.0f;
+	public float lenghtBackground = 41.39f;
+	float nextBackgroundPosition = 93.79f;
 
 	public GameObject[] blocks;
 
