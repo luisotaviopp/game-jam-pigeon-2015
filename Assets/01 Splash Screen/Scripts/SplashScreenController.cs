209debdfa957adae423af26ad7da375c1798e066
edu4hd0 <luizeduardo.ifsc@gmail.com>
2015-04-26 20:45:07 -0300

    mudanca na splash screen... adicionado feature de colocar várias imagens de splash screen

--- Assets/01 Splash Screen/Scripts/SplashScreenController.cs
+++ Assets/01 Splash Screen/Scripts/SplashScreenController.cs
@@ -3,7 +3,49 @@ using System.Collections;
 
 public class SplashScreenController : MonoBehaviour 
 {
-	public float delayMinNextScene = 3.0f;
+    public GameObject[] logo;
+    public float logoTime = 2;
+    float counter = 0;
+    int logoIndex = 0;
+    string nextLevel = "Main Menu";
+    // Update is called once per frame
+
+    void Start()
+    {
+        for (int i = 0; i < logo.Length; i++)
+        {
+            ShowHide(i, false);
+        }
+        ShowHide(0, true);
+    }
+
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter > logoTime)
+        {
+            logoIndex++;
+            if (logoIndex > logo.Length - 1)
+            {
+                Application.LoadLevel(nextLevel);
+                return;
+            }
+            ShowHide(logoIndex-1, false);
+            ShowHide(logoIndex, true);
+            counter = 0;
+        }
+    }
+
+
+    void ShowHide(int index, bool state)
+    {
+        logo[index].SetActive(state);
+    }
+    
+    
+    
+    /*
+    public float delayMinNextScene = 3.0f;
 	public string nextSceneName = "Main Menu";
 
 	void Start () 
@@ -35,4 +77,5 @@ public class SplashScreenController : MonoBehaviour
 
 		status.allowSceneActivation = true;
 	}
+     * */
 }
