640f189a6da44cf1fc45ca47740be5b96b3bce1d
Pedro Bacchini <engenharia_bacchini@hotmail.com>
2015-04-26 21:18:11 -0300

    Main Menu iniciando Gameplay!

---
+++ Assets/02 Menu/Scripts/NextScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextScene : MonoBehaviour 
+{
+
+	public void NameScene(string nameScene)
+	{
+		Application.LoadLevel (nameScene);
+	}
+}
