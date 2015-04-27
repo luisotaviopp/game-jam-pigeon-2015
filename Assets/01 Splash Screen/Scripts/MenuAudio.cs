5941f3f5f144c4fa0a3d2eb20e78b4ee0a5abd45
Luiz Eduardo Ramos <luizeduardo.ifsc@gmail.com>
2015-04-26 23:42:10 -0300

    Audio do menu

---
+++ Assets/01 Splash Screen/Scripts/MenuAudio.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuAudio : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (FindObjectsOfType<MenuAudio>().Length > 1)
+        {
+            DestroyImmediate(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+	}
+
+    void LateUpdate(){
+        if (Application.loadedLevelName == "Gameplay")
+            DestroyImmediate(gameObject);
+    }
+}
